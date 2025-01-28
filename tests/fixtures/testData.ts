a9ce9c8bedf0c45a9104adfde2785b0668574675
Evzen <evgenii.sitkin@pbktechnology.cz>
2025-01-28 20:45:15 +0100

    added readonly

--- tests/fixtures/testData.ts
+++ tests/fixtures/testData.ts
@@ -1,8 +1,8 @@
 export class testData {
-	static baseUrl: string = process.env.BASE || '';
-	static loginUrl: string = `${testData.baseUrl}/login`;
-	static basketUrl: string = `${testData.baseUrl}/basket`;
-	static clearBasketUrl: string = `${testData.baseUrl}/basket/clear`;
+	static readonly baseUrl: string = process.env.BASE || '';
+	static readonly loginUrl: string = `${testData.baseUrl}/login`;
+	static readonly basketUrl: string = `${testData.baseUrl}/basket`;
+	static readonly clearBasketUrl: string = `${testData.baseUrl}/basket/clear`;
 
 	static userLogin: string = process.env.LOGIN || '';
 	static userPassword: string = process.env.PASSWORD || '';
